db63fbd70c983dc7b4ab1199cbfdfdb0ecda19f2
24DP1RSAVC <A240740RS@rvt.lv>
2025-09-22 12:33:39 +0300

    tax payer

---
+++ src/main/java/rvt/GiftTax.java
@@ -0,0 +1,13 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class GiftTax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Value of the gift?");
+        int value = Integer.valueOf(scanner.nextLine());
+        if (value <= 5000)
+    }
+}
