2976febe4b980a2d6fcfc9744f38bb4432258bfa
24DP1RSAVC <A240740RS@rvt.lv>
2025-09-24 09:12:09 +0300

    Gift Tax

--- src/main/java/rvt/GiftTax.java
+++ src/main/java/rvt/GiftTax.java
@@ -8,6 +8,18 @@ public class GiftTax {
 
         System.out.println("Value of the gift?");
         int value = Integer.valueOf(scanner.nextLine());
-        if (value <= 5000)
+        if (value < 5000){
+            System.out.println("No tax");
+        }else if(value >= 5000 && value < 25000){
+            System.out.println("Tax " +((double)100 + (value - 5000) * 0.08));
+        }else if(value >= 25000 && value < 55000){
+            System.out.println("Tax " +((double)1700 + (value - 25000) * 0.10));
+        }else if(value >= 55000 && value < 200000){
+            System.out.println("Tax " +((double)4700 + (value - 55000) * 0.12));
+        }else if(value >= 200000 && value < 1000000){
+            System.out.println("Tax " +((double)22100 + (value - 200000) * 0.15));
+        }else{
+            System.out.println("Tax " +((double)14200 + (value - 1000000) * 0.17));
+        }
     }
 }
