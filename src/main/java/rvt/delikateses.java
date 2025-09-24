6f9a81e4331b9ca537d93dad752cf75b3be30de5
24DP1RSAVC <A240740RS@rvt.lv>
2025-09-24 10:08:09 +0300

    delikatese

---
+++ src/main/java/rvt/delikateses.java
@@ -0,0 +1,32 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class delikateses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadi preci: ");
+        String prece = scanner.nextLine();
+
+        System.out.println("Ievadi cenu: ");
+        double cena = Double.valueOf(scanner.nextLine());
+
+        System.out.println("Ekspress piegāde (0 == nē, 1 == jā): ");
+        int ekspress = Integer.valueOf(scanner.nextLine());
+
+        double piegade = 0.00;
+        if (cena > 10.00){
+            piegade = 2.00;
+        }if(ekspress == 1){
+            piegade += 3.0;
+        }
+        double kopa = cena + piegade;
+
+        System.out.println("\nRēķins:");
+        System.out.println(prece, cena);
+        System.out.println(piegade);
+        System.out.println(kopa);
+      
+    }
+}
--- src/test/java/rvt/App.java
+++ src/test/java/rvt/App.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         // int age = Integer.valueOf(scanner.nextLine());
         // if (age >= 18) {
         //     System.out.println("Tu esi pilngadīgs");
         // } else {
         //     System.out.println("Tu nevari lietot programmu!");
-        // }  
+        // } 
+
         System.out.println("Password?");
         String password = scanner.nextLine();
 
