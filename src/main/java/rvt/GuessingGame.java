7864c17a7553db716a526487028946c832bc2e89
Roberts Savčenko <A240740RS@rvt.lv>
2025-09-30 21:21:24 +0300

    Spēle

--- src/main/java/rvt/GuessingGame.java
+++ src/main/java/rvt/GuessingGame.java
@@ -6,25 +6,30 @@ import java.util.Scanner;
 public class GuessingGame {
     public static void main(String[] args) {
         Random rand = new Random();
-        int result = rand.nextInt(9) + 1;
+        int result = rand.nextInt(10) + 1; // ģenerē 1..10
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Es domāju par skaitli no 1 līdz 10.");
         System.out.println("Tev tas jāuzmin trīs mēģinājumos");
         System.out.println("Ievadi minējumu:");
 
-        int num = 0;
-        int number = Integer.valueOf(scanner.nextLine());
-        while(number < 3)
-        if(number == result){
-            System.out.println("PAREIZI!");
-            break;
-        }else{
-            System.out.println("NEPAREIZI!");
-            num += 1;
-            if(number == 3){
+        boolean won = false;
+        for (int attempt = 1; attempt <= 3; attempt++) {
+            int guess = Integer.valueOf(scanner.nextLine());
 
+            if (guess == result) {
+                System.out.println("PAREIZI!");
+                System.out.println("Tu esi uzvarējis spēli.");
+                won = true;
+                break;
+            } else {
+                System.out.println("nepareizi");
+                if (attempt == 3) {
+                    System.out.println("Pareizais skaitlis bija " + result + ".");
+                    System.out.println("Tu esi zaudējis spēli.");
+                }
             }
         }
     }
 }
+
